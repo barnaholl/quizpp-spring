f63ed20be363448442b45738514e19ce49eda1b7
barnaholl <hollbarna@freemail.hu>
2020-09-08 10:47:56 +0200

    Question Entity

---
+++ question-handler-service/src/main/java/com/theyellowpug/questionhandlerservice/entity/Question.java
@@ -0,0 +1,44 @@
+package com.theyellowpug.questionhandlerservice.entity;
+
+import com.sun.istack.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Question {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(nullable = false)
+    private String question;
+
+    @Column(nullable = false)
+    private String wrongAnswer1;
+
+    @Column(nullable = false)
+    private String wrongAnswer2;
+
+    @Column(nullable = false)
+    private String wrongAnswer3;
+
+    @Column(nullable = false)
+    private String correctAnswer;
+
+    @Column(nullable = false)
+    private short difficulty;
+
+
+}
