c134ff1791e604ec065d615934c19e6a47f3cdab
barnaholl <hollbarna@freemail.hu>
2020-09-08 10:14:10 +0200

    create question-handler-service

---
+++ question-handler-service/src/main/java/com/theyellowpug/questionhandlerservice/QuestionHandlerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.theyellowpug.questionhandlerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestionHandlerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestionHandlerServiceApplication.class, args);
+	}
+
+}
---
+++ question-handler-service/src/test/java/com/theyellowpug/questionhandlerservice/QuestionHandlerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.theyellowpug.questionhandlerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionHandlerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
