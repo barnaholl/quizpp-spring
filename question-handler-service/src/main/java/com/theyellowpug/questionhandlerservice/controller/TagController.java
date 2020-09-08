441ca87088a911fbe6edff00a6a7ec95c1066b5f
barnaholl <hollbarna@freemail.hu>
2020-09-08 15:35:08 +0200

    addTag()

---
+++ question-handler-service/src/main/java/com/theyellowpug/questionhandlerservice/controller/TagController.java
@@ -0,0 +1,23 @@
+package com.theyellowpug.questionhandlerservice.controller;
+
+import com.theyellowpug.questionhandlerservice.entity.Tag;
+import com.theyellowpug.questionhandlerservice.repository.TagRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/tag")
+public class TagController {
+    @Autowired
+    private TagRepository tagRepository;
+
+    @PostMapping("")
+    public String addTag(@RequestBody Tag tag){
+        tagRepository.save(tag);
+        return "Tag: "+tag.getValue()+" is successfully saved";
+    }
+
+
+}
