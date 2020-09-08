9b75d824724d25fdc27f8dcd71682ccadc330683
barnaholl <hollbarna@freemail.hu>
2020-09-08 18:13:44 +0200

    game_handler database connection

--- game-handler-service/src/main/java/com/theyellowpug/gamehandlerservice/GameHandlerServiceApplication.java
+++ game-handler-service/src/main/java/com/theyellowpug/gamehandlerservice/GameHandlerServiceApplication.java
@@ -2,8 +2,10 @@ package com.theyellowpug.gamehandlerservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class GameHandlerServiceApplication {
 
 	public static void main(String[] args) {
