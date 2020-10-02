04fc91860699766be9eca971de890311d5e8bf36
barnaholl <hollbarna@freemail.hu>
2020-10-02 13:32:20 +0200

    createSession now init the first question

--- game-session-handler/src/main/java/com/theyellowpug/gamesessionhandler/controller/SoloGameSessionController.java
+++ game-session-handler/src/main/java/com/theyellowpug/gamesessionhandler/controller/SoloGameSessionController.java
@@ -2,6 +2,7 @@ package com.theyellowpug.gamesessionhandler.controller;
 
 import com.theyellowpug.gamesessionhandler.entity.SoloGameSession;
 import com.theyellowpug.gamesessionhandler.repository.SoloGameSessionRepository;
+import com.theyellowpug.gamesessionhandler.service.QuestionServiceCaller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,12 +16,21 @@ public class SoloGameSessionController {
 
     @Autowired
     private SoloGameSessionRepository soloGameSessionRepository;
+    @Autowired
+    QuestionServiceCaller questionServiceCaller;
 
     @PostMapping("/{gameId}/{tag}/{difficulty}")
-    public String createSoloGameSession(@PathVariable("gameId") Long gameId,@PathVariable("difficulty") Short difficulty,@PathVariable("tag") String tag){
-        SoloGameSession gameSession = SoloGameSession.builder().gameId(gameId).difficulty(difficulty).tag(tag).isActive(true).currentRound((short) 1).build();
+    public Long createSoloGameSession(@PathVariable("gameId") Long gameId,@PathVariable("difficulty") Short difficulty,@PathVariable("tag") String tag){
+        SoloGameSession gameSession = SoloGameSession.builder()
+                .gameId(gameId)
+                .difficulty(difficulty)
+                .tag(tag)
+                .isActive(true)
+                .currentRound((short) 1)
+                .currentQuestion(questionServiceCaller.getQuestionId(tag, difficulty))
+                .build();
         soloGameSessionRepository.save(gameSession);
-        return "Game session with id:"+gameId+" tag:"+tag+" difficulty:"+difficulty+" has been created";
+        return gameSession.getId();
     }
 
     @GetMapping("/{id}")
