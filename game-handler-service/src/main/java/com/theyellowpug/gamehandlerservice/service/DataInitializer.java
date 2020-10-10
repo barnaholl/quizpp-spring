3ae483f90e74fcde8406a70f9f8ddc69de42a7ab
barnaholl <hollbarna@freemail.hu>
2020-10-10 18:50:20 +0200

    game-handler-service DataInitializer

---
+++ game-handler-service/src/main/java/com/theyellowpug/gamehandlerservice/service/DataInitializer.java
@@ -0,0 +1,25 @@
+package com.theyellowpug.gamehandlerservice.service;
+
+import com.theyellowpug.gamehandlerservice.entity.Game;
+import com.theyellowpug.gamehandlerservice.repository.GameRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DataInitializer implements CommandLineRunner {
+    private final GameRepository gameRepository;
+
+    public DataInitializer(GameRepository gameRepository) {
+        this.gameRepository = gameRepository;
+    }
+
+    public  void createGame(String title,String description,String tag,String type,short difficulty){
+        Game game= Game.builder().title(title).description(description).tag(tag).type(type).difficulty(difficulty).build();
+        gameRepository.save(game);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        createGame("My first game ever","This is the first game of QuizPP","general","free",(short)1);
+    }
+}
