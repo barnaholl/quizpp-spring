6f63197d8184c3c47304a458bc7136bbefd72656
barnaholl <hollbarna@freemail.hu>
2020-10-11 10:26:33 +0200

    Question model

---
+++ question-handler-service/src/main/java/com/theyellowpug/questionhandlerservice/model/Question.java
@@ -0,0 +1,21 @@
+package com.theyellowpug.questionhandlerservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Question {
+    private Long id;
+
+    private String question;
+
+    private String answer1;
+    private String answer2;
+    private String answer3;
+    private String answer4;
+}
