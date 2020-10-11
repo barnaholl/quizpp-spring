08bdb40e0bf64bf260b5b9af0db5f63588426acf
barnaholl <hollbarna@freemail.hu>
2020-10-11 20:06:13 +0200

    user-handler-service

---
+++ user-handler-service/src/main/java/com/theyellowpug/userhandlerservice/UserHandlerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.theyellowpug.userhandlerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserHandlerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserHandlerServiceApplication.class, args);
+	}
+
+}
---
+++ user-handler-service/src/test/java/com/theyellowpug/userhandlerservice/UserHandlerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.theyellowpug.userhandlerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserHandlerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
