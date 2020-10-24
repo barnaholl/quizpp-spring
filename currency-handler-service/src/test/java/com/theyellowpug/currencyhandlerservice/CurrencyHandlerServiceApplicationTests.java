8fea135d79b1b30f3518d8487a52d6e97f60dd98
barnaholl <hollbarna@freemail.hu>
2020-10-24 12:09:04 +0200

    CurrencyHandlerService init

---
+++ currency-handler-service/src/test/java/com/theyellowpug/currencyhandlerservice/CurrencyHandlerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.theyellowpug.currencyhandlerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyHandlerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
